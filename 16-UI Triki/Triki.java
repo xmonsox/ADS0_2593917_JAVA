d17f34e891639b3d67349ef946422dbe840120e4
MaribelOJ <maribel-obando.j@utp.edu.co>
2023-05-08 11:46:41 -0500

    Lunes 08/05/2022 Error-no me muestra el turno de la IA

--- 16-UI Triki/Triki.java
+++ 16-UI Triki/Triki.java
@@ -92,22 +92,123 @@ public class Triki extends JFrame{
 		revalidate();
 	}
 
-	public void marcarCasilla(int fila, int columna){
+	public void marcarCasillaUser(int fila, int columna){
 		if( this.tablero_interno[fila][columna]=='-'){
-			char ficha = (this.turno==1)? 'X':'0';
-			this.tablero_interno[fila][columna] = ficha;
-			this.tablero_lbls[fila][columna].setForeground( (this.turno==1)? Color.black:Color.red );
+			System.out.println("U");
+			marcarCasilla(fila, columna);
+			ganoPartida();
+			
 			this.turno = (this.turno%2)+1;
-			this.imprimirTablero();
+			if(this.turno==2 && this.casillaVacia()){
+				System.out.println("M");
+				this.validaMaquina();
+				
+				ganoPartida();
+				this.turno = (this.turno%2)+1;
+			}
+
 		}else{
 			System.out.println("La Posicion es invalida.");
 		}
-
 	}
-	public void ganoPartida(){
-		if(this.tablero_interno[0][0] == 'X' && this.tablero_interno[1][0] == 'X' && this.tablero_interno[2][0] == 'X'){
-			System.out.println("Gano USu");
 
+	public boolean casillaVacia(){
+		boolean correcto = false;
+		for (int i=0;i<this.tablero_interno.length;i++){
+			for(int j=0;j<this.tablero_interno[i].length;j++){
+				if(this.tablero_interno[i][j] == '-'){
+					correcto = true;
+					break;
+				}
+			}
 		}
+		return correcto;
+	}
+	public void marcarCasilla(int fila, int columna){
+		char ficha = (this.turno==1)? 'X':'0';
+		this.tablero_interno[fila][columna] = ficha;
+		this.tablero_lbls[fila][columna].setForeground( (this.turno==1)? Color.black:Color.red );
+		this.imprimirTablero();
+	}
+
+	public void ganoPartida(){
+		for (int i = 0; i < 3; i++) {
+			if (tablero_interno[i][0] == tablero_interno[i][1] && tablero_interno[i][0] == tablero_interno[i][2] && tablero_interno[i][0] != '-') {
+				ganador = (tablero_interno[i][0] == 'X') ? 1 : 2;
+				System.out.println("El ganador es el jugador " + ganador);
+				return;
+			}
+		}
+		for (int j = 0; j < 3; j++) {
+			if (tablero_interno[0][j] == tablero_interno[1][j] && tablero_interno[0][j] == tablero_interno[2][j] && tablero_interno[0][j] != '-') {
+				ganador = (tablero_interno[0][j] == 'X') ? 1 : 2;
+				System.out.println("El ganador es el jugador " + ganador);
+				return;
+			}
+		}
+		if (tablero_interno[0][0] == tablero_interno[1][1] && tablero_interno[0][0] == tablero_interno[2][2] && tablero_interno[0][0] != '-') {
+			ganador = (tablero_interno[0][0] == 'X') ? 1 : 2;
+			System.out.println("El ganador es el jugador " + ganador);
+			return;
+		}
+		if (tablero_interno[0][2] == tablero_interno[1][1] && tablero_interno[0][2] == tablero_interno[2][0] && tablero_interno[0][2] != '-') {
+			ganador = (tablero_interno[0][2] == 'X') ? 1 : 2;
+			System.out.println("El ganador es el jugador " + ganador);
+			return;
+		}
+	}
+	public void validaMaquina(){
+		if(tablero_interno[0][0] == '0' && tablero_interno[0][1] == '0' && tablero_interno[0][2] == '-'){
+			marcarCasilla(0,2);
+		}else if(tablero_interno[0][0] == '0' && tablero_interno[0][2] == '0' && tablero_interno[0][1] == '-'){
+			marcarCasilla(0,1);	
+		}else if(tablero_interno[0][2] == '0' && tablero_interno[0][1] == '0' && tablero_interno[0][0] == '-'){
+			marcarCasilla(0,0);	
+		}else if(tablero_interno[1][0] == '0' && tablero_interno[1][1] == '0' && tablero_interno[1][2] == '-'){
+			marcarCasilla(1,2);	
+		}else if(tablero_interno[1][0] == '0' && tablero_interno[1][2] == '0' && tablero_interno[1][1] == '-'){
+			marcarCasilla(1,1);	
+		}else if(tablero_interno[1][2] == '0' && tablero_interno[1][1] == '0' && tablero_interno[1][0] == '-'){
+			marcarCasilla(1,0);	
+		}else if(tablero_interno[2][0] == '0' && tablero_interno[2][1] == '0' && tablero_interno[2][2] == '-'){
+			marcarCasilla(2,2);	
+		}else if(tablero_interno[2][0] == '0' && tablero_interno[2][2] == '0' && tablero_interno[2][1] == '-'){
+			marcarCasilla(2,1);	
+		}else if(tablero_interno[2][1] == '0' && tablero_interno[2][2] == '0' && tablero_interno[2][0] == '-'){
+			marcarCasilla(2,0);	
+		}else if(tablero_interno[0][0] == '0' && tablero_interno[1][0] == '0' && tablero_interno[2][0] == '-'){
+			marcarCasilla(2,0);	
+		}else if(tablero_interno[0][0] == '0' && tablero_interno[2][0] == '0' && tablero_interno[1][0] == '-'){
+			marcarCasilla(1,0);	
+		}else if(tablero_interno[2][0] == '0' && tablero_interno[1][0] == '0' && tablero_interno[0][0] == '-'){
+			marcarCasilla(0,0);	
+		}else if(tablero_interno[0][1] == '0' && tablero_interno[1][1] == '0' && tablero_interno[2][1] == '-'){
+			marcarCasilla(2,1);	
+		}else if(tablero_interno[2][1] == '0' && tablero_interno[0][1] == '0' && tablero_interno[1][1] == '-'){
+			marcarCasilla(1,1);	
+		}else if(tablero_interno[2][1] == '0' && tablero_interno[1][1] == '0' && tablero_interno[0][1] == '-'){
+			marcarCasilla(0,1);	
+		}else if(tablero_interno[0][2] == '0' && tablero_interno[1][2] == '0' && tablero_interno[2][2] == '-'){
+			marcarCasilla(2,2);	
+		}else if(tablero_interno[0][2] == '0' && tablero_interno[2][2] == '0' && tablero_interno[1][2] == '-'){
+			marcarCasilla(1,2);	
+		}else if(tablero_interno[2][2] == '0' && tablero_interno[1][2] == '0' && tablero_interno[0][2] == '-'){
+			marcarCasilla(0,2);	
+		}else if(tablero_interno[0][0] == '0' && tablero_interno[1][1] == '0' && tablero_interno[2][2] == '-'){
+			marcarCasilla(2,2);	
+		}else if(tablero_interno[0][0] == '0' && tablero_interno[2][2] == '0' && tablero_interno[1][1] == '-'){
+			marcarCasilla(1,1);	
+		}else if(tablero_interno[1][1] == '0' && tablero_interno[2][2] == '0' && tablero_interno[0][0] == '-'){
+			marcarCasilla(0,0);	
+		}else if(tablero_interno[2][0] == '0' && tablero_interno[1][1] == '0' && tablero_interno[0][2] == '-'){
+			marcarCasilla(0,2);	
+		}else if(tablero_interno[2][0] == '0' && tablero_interno[0][2] == '0' && tablero_interno[1][1] == '-'){
+			marcarCasilla(1,1);	
+		}else if(tablero_interno[1][1] == '0' && tablero_interno[0][2] == '0' && tablero_interno[2][0] == '-'){
+			marcarCasilla(2,0);	
+		}else if(tablero_interno[0][2] == '0' && tablero_interno[0][1] == '0' && tablero_interno[0][0] == '-'){
+			marcarCasilla(0,1);	
+		}
+
 	}
 }
